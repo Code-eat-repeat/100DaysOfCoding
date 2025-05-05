65029b78ab4916224836a8f82179af4366a26584
Simran Ahuja <Simranjoin1998@gmail.com>
2025-05-05 21:39:15 +0530

    Day4 Done Only Two Questions

---
+++ src/dsa/practice/day4/RotateImageBy90.java
@@ -0,0 +1,33 @@
+package dsa.practice.day4;
+
+public class RotateImageBy90 {
+    public static void main(String[] args){
+        int[][] matrix={{0, 1, 1, 2}, {2, 0, 3, 1}, {4, 5, 0, 5}, {5, 6, 7, 0}};
+        rotate(matrix);
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix.length;j++){
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void rotate(int[][] matrix){
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i;j<matrix[0].length;j++){
+                int temp=0;
+                temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix.length/2;j++){
+                int temp=0;
+                temp=matrix[i][j];
+                matrix[i][j]=matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j] =temp;
+            }
+        }
+    }
+}
---
+++ src/dsa/practice/day4/SprialTraversalOfMatrix.java
@@ -0,0 +1,45 @@
+package dsa.practice.day4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SprialTraversalOfMatrix {
+
+    public static void main(String[] args){
+        int[][] matrix = {{ 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }};
+        List<Integer> result = printSpiral(matrix);
+        for(int i=0;i<result.size();i++){
+            System.out.print(result.get(i));
+        }
+    }
+
+    private static List<Integer> printSpiral(int[][] mat){
+        List<Integer> ans = new ArrayList<>();
+        int n=mat.length;
+        int m=mat[0].length;
+        int top=0,left=0,bottom=n-1,right=m-1;
+        while(top<=bottom && left<=right){
+            for(int i=left; i<=right;i++){
+                ans.add(mat[top][i]);
+            }
+            top++;
+            for(int i=top;i<=bottom;i++)ans.add(mat[i][right]);
+            right--;
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    ans.add(mat[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    ans.add(mat[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+}
